78a553e1fdd4569f0af7f2c8bd085c82c7b0e22b
Orlando Curieles <orlando.curieles@ingeint.com>
2020-05-18 09:04:04 -0500

    Update module

--- models/partner.py
+++ models/partner.py
@@ -6,8 +6,8 @@ from odoo import api, fields, models
 class PymesPartner(models.Model):
     _inherit = 'res.partner'
 
-    bank_id = fields.Many2one('res.bank', string='Bank')
-    account_id = fields.Many2one('res.partner.bank', string='Bank Account')
+    #bank_id = fields.Many2one('res.bank', string='Bank')
+    #account_id = fields.Many2one('res.partner.bank', string='Bank Account')
 
 
 
