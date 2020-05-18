3d0253628f4c0b669be671049e6685407ed48e5a
Orlando Curieles <orlando.curieles@ingeint.com>
2020-05-18 16:25:00 -0500

    update res_user created

--- models/partner.py
+++ models/partner.py
@@ -6,4 +6,5 @@ from odoo import api, fields, models
 class Partner(models.Model):
     _inherit = 'res.partner'
 
-    #company_specs = fields.Char('company_specs')
+    company_specs = name = fields.Char()
+
--- models/users.py
+++ models/users.py
@@ -5,3 +5,16 @@ class User(models.Model):
     _inherit = 'res.users'
 
     tag_ids = fields.Many2many('product.tag', string='Tag Pyme')
+
+    @api.constrains('share')
+    def _updateUser(self):
+        for record in self:
+            if not record.share:
+                record.env.cr.execute('update res_partner set create_uid=%s where id = %s', [record.id, record.partner_id.id])
+                record.env['res.partner'].invalidate_cache()
+
+
+
+
+
+
